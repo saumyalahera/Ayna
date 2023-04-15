7ac2cca7f777183497278f8557005959a26150f4
Saumya <saumyalahera@gmail.com>
2023-04-15 04:31:01 -0700

    Initial setup

---
+++ Ayna/Ayna/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Ayna
+//
+//  Created by Saumya Lahera on 4/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
